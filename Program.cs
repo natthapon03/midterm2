9f80079cd8a63c786b8edf39dfb9c3a0c7eb6cf9
natthapon03 <pinnatthapon@gmail.com>
2023-03-13 17:23:53 +0700

    m

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿namespace m{
+    class program{
+        static void Main(string[] args){
+            Console.WriteLine("dayofweek : ");
+            int d = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("timeofday : ");
+            int t = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("menu : ");
+            string m = (Console.ReadLine());
+
+
+            if(d <=7 && d >=1 ){
+                Console.WriteLine();
+
+            if(t <=8 && t >=18){
+                Console.WriteLine();
+
+            if(m =="coffee"){
+                Console.WriteLine(" ");
+
+            } else {
+                Console.WriteLine("Please enter a valid menu");
+
+            }
+            } else if(m =="weekendset"){
+                Console.WriteLine(" ");
+
+            } else {
+                Console.WriteLine("Please enter a valid menu");
+            
+            }
+            } else if(m == "breakfastset"){
+                Console.WriteLine(" ");
+
+            } else {
+                Console.WriteLine("Please enter a valid menu");
+            }
+
+
+                  
+
+        }
+
+           
+
+    }
+
+            
+                
+}
